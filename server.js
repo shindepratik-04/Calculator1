9da57e0c482f653678a6bd6cb90e988e948e27db
Pratik Shinde <55885193+shindepratik-04@users.noreply.github.com>
2021-05-17 10:52:32 +0530

    changed formula

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get("/", function(req, res){
 app.post("/", function(req, res){
   var n1 = Number(req.body.num1);
   var n2 = Number(req.body.num2);
-  var result = n1 + n2;
+  var result = n1/(n2*n2);
   res.send("<h1 style='height: 25vh; position: relative;margin: 20px;margin-left: 150px;'>The Answer is: " + result + "</h1>");
 });
 
